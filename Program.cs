332becad1028af9fd0f34c28404afdaa95423b30
kutawhat <sergey.kovyazo@gmail.com>
2023-11-27 16:20:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,194 @@
+﻿using System.Threading;
+using System.Text;
+using System.IO;
+using System;
+
+namespace christmas_tree
+{
+    internal class Program
+    {
+        static ConsoleColor[] colors = (ConsoleColor[]) ConsoleColor.GetValues(typeof(ConsoleColor));
+        
+
+        public static void PrintTreeAnimatedDemo1(string tree)
+        {
+            while (1 == 1)
+            {
+                Thread.Sleep(1200);
+
+                for (int i = 0; i < tree.Length; i++)
+                {
+                    if (tree[i] == '*')
+                    {
+                        Console.ForegroundColor = colors[8];
+                        Console.Write(tree[i]);
+                    }
+                    else if (tree[i] == 'o')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(tree[i]);
+                    }
+                    else if (tree[i] == Convert.ToChar(39))
+                    {
+                        Console.ForegroundColor = colors[14];
+                        Console.Write(tree[i]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = colors[8];
+                        Console.Write(tree[i]);
+                    }
+                }
+
+                Thread.Sleep(1200);
+                
+                for (int i = 0; i < tree.Length; i++)
+                {
+                    if (tree[i] == '*')
+                    {
+                        Console.ForegroundColor = colors[4];
+                        Console.Write(tree[i]);
+                    }
+                    else if (tree[i] == 'o')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(tree[i]);
+                    }
+                    else if (tree[i] == Convert.ToChar(39))
+                    {
+                        Console.ForegroundColor = colors[7];
+                        Console.Write(tree[i]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = colors[8];
+                        Console.Write(tree[i]);
+                    }
+                }
+            }
+        }
+        
+        public static void PrintTreeAnimatedDemo2(string tree)
+        {
+            while (1 == 1)
+            {
+                Thread.Sleep(700);
+
+                for (int i = 0; i < tree.Length; i++)
+                {
+                    if (tree[i] == '*')
+                    {
+                        Console.ForegroundColor = colors[8];
+                        Console.Write(tree[i]);
+                    }
+                    else if (tree[i] == 'o')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(tree[i]);
+                    }
+                    else if (tree[i] == '&')
+                    {
+                        Console.ForegroundColor = colors[14];
+                        Console.Write(tree[i]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = colors[8];
+                        Console.Write(tree[i]);
+                    }
+                }
+
+                Thread.Sleep(700);
+                
+                for (int i = 0; i < tree.Length; i++)
+                {
+                    if (tree[i] == '*')
+                    {
+                        Console.ForegroundColor = colors[4];
+                        Console.Write(tree[i]);
+                    }
+                    else if (tree[i] == 'o')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(tree[i]);
+                    }
+                    else if (tree[i] == '&')
+                    {
+                        Console.ForegroundColor = colors[8];
+                        Console.Write(tree[i]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = colors[8];
+                        Console.Write(tree[i]);
+                    }
+                }
+            }
+        }
+        public static void PrintTreeAnimated(string tree)
+        {
+            while (!Console.KeyAvailable)
+            {
+                Thread.Sleep(700);
+
+                for (int i = 0; i < tree.Length; i++)
+                {
+                    if (tree[i] == '*')
+                    {
+                        Console.ForegroundColor = colors[8];
+                        Console.Write(tree[i]);
+                    }
+                    else if (tree[i] == '"')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(tree[i]);
+                    }
+                    else if (tree[i] == 'o')
+                    {
+                        Console.ForegroundColor = colors[4];
+                        Console.Write(tree[i]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = colors[7];
+                        Console.Write(tree[i]);
+                    }
+                }
+
+                Thread.Sleep(700);
+                
+                for (int i = 0; i < tree.Length; i++)
+                {
+                    if (tree[i] == '*')
+                    {
+                        Console.ForegroundColor = colors[14];
+                        Console.Write(tree[i]);
+                    }
+                    else if (tree[i] == '"')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(tree[i]);
+                    }
+                    else if (tree[i] == 'o')
+                    {
+                        Console.ForegroundColor = colors[8];
+                        Console.Write(tree[i]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = colors[7];
+                        Console.Write(tree[i]);
+                    }
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            string path = "/Users/ku/Documents/coding/archive/csh/christmas_tree/tree.txt";
+            string tree = File.ReadAllText(path);
+
+            PrintTreeAnimated(tree);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+Merry Christmas and HNY everyone!
+This code will add an animated Christmas tree at your terminal.
+
+Don't forget to change path to the tree.txt file in the code.
+
+## What to do then:
+2. Open the Terminal and cd to the directive with file
+5. Type in Terminal:
+```bash
+csc Program.cs
+mono Program.exe
+```
